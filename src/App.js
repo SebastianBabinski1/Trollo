1bfd3440e54832e2d19c69ae32cf010e69256bc4
Sebastian B <sebastian.babinski1@gmail.com>
2021-10-03 19:38:08 +0200

    Change some components from class to function

--- src/App.js
+++ src/App.js
@@ -3,51 +3,44 @@
 // import { TableSVG } from './svg/TableSVG';
 // import BoardHeader from './components/BoardHeader';
 import List from "./components/List/List.jsx";
-import React from "react";
+import React, { useState } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
 // Here u must make it function component and make dnd drop logic
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleAddingList = this.handleAddingList.bind(this);
-    this.handleLists = this.handleLists.bind(this);
-    this.state = {
-      lists: [
+const App = () => {
+  const [lists, setLists] = useState([
+    {
+      title: "First Episode",
+      id: 0,
+      cards: [
         {
-          title: "First Episode",
           id: 0,
-          cards: [
-            {
-              id: 0,
-              text: "Im a first card of first list",
-            },
-            {
-              id: 1,
-              text: "Im a second card of first list",
-            },
-          ],
+          text: "Im a first card of first list",
         },
         {
-          title: "Second Episode",
           id: 1,
-          cards: [
-            {
-              id: 0,
-              text: "Im a first card of second list",
-            },
-            {
-              id: 1,
-              text: "Im a second card of second list",
-            },
-          ],
+          text: "Im a second card of first list",
         },
       ],
-    };
-  }
+    },
+    {
+      title: "Second Episode",
+      id: 1,
+      cards: [
+        {
+          id: 0,
+          text: "Im a first card of second list",
+        },
+        {
+          id: 1,
+          text: "Im a second card of second list",
+        },
+      ],
+    },
+  ]);
 
-  handleAddingCard(listID, cardText) {
+  const handleAddingCard = (listID, cardText) => {
     const handleCardCounter = (list) => {
       let calculatedID = 0;
       list.cards.forEach((card) => {
@@ -58,34 +51,39 @@ class App extends React.Component {
       return calculatedID;
     };
 
-    this.state.lists.forEach((item) => {
+    lists.forEach((item, index) => {
       if (item.id === listID) {
-        this.setState({
-          cards: item.cards.push({
-            id: handleCardCounter(item),
-            text: cardText,
-          }),
+        let newArray = [...lists];
+        let newCards = lists[index].cards.concat({
+          id: handleCardCounter(item),
+          text: cardText,
         });
+        newArray[index].cards = newCards;
+        setLists(newArray);
       }
     });
-  }
+  };
 
-  handleRemovingCard(cardID, listID) {
-    this.state.lists.forEach((item) => {
+  const handleRemovingCard = (cardID, listID) => {
+    lists.forEach((item, indexofArray) => {
       if (item.id === listID) {
-        item.cards.forEach((card, index) => {
+        item.cards.forEach((card, indexofCard) => {
           if (card.id === cardID) {
-            this.setState({ cards: item.cards.splice(index, 1) });
+            let newArray = [...lists];
+            let cardsAfterRemoving = item.cards;
+            cardsAfterRemoving.splice(indexofCard, 1);
+            newArray[indexofArray].cards = cardsAfterRemoving;
+            setLists(newArray);
           }
         });
       }
     });
-  }
+  };
 
-  handleAddingList() {
+  const handleAddingList = () => {
     const handleListCounter = () => {
       let calculatedID = 0;
-      this.state.lists.forEach((list) => {
+      lists.forEach((list) => {
         if (list.id >= calculatedID) {
           calculatedID = list.id + 1;
         }
@@ -93,58 +91,54 @@ class App extends React.Component {
       return calculatedID;
     };
 
-    this.setState({
-      lists: [
-        ...this.state.lists,
-        {
-          title: "",
-          id: handleListCounter(),
-          cards: [],
-        },
-      ],
-    });
-  }
+    setLists([
+      ...lists,
+      {
+        title: "",
+        id: handleListCounter(),
+        cards: [],
+      },
+    ]);
+  };
 
-  handleLists() {
+  const handleLists = () => {
     const tableOfLists = [];
-    this.state.lists.forEach((item) => {
+    Array.from(lists).forEach((item) => {
       tableOfLists.push(
         <List
           key={item.id}
           listID={item.id}
           cards={item.cards}
           title={item.title}
-          handleAddingCard={this.handleAddingCard.bind(this)}
-          handleRemovingCard={this.handleRemovingCard.bind(this)}
+          handleAddingCard={handleAddingCard}
+          handleRemovingCard={handleRemovingCard}
         />
       );
     });
     return tableOfLists;
-  }
+  };
 
-  render() {
-    return (
-      <DndProvider backend={HTML5Backend}>
-        <div className="App h-screen bg-red-100">
-          <div className="App-header">
-            {/* Here u must do smth with svg files, cause there are errors */}
-            {/* <Navbar/> */}
-            {/* <Button spanClass="pl-2" buttonClass="flex p-1 m-2 bg-black bg-opacity-10 rounded-md hover:bg-opacity-20" svg={TableSVG} text="Sample button"/> */}
-            {/* <BoardHeader tableName="Name of table"/> */}
-          </div>
-          <div className="App-content flex">
-            {this.handleLists()}
-            <button
-              className="border-2 px-2 rounded-r-md shadow-md w-1/4 h-8 mt-2 bg-white opacity-70"
-              onClick={this.handleAddingList}
-            >
-              Add new list
-            </button>
-          </div>
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="App h-screen bg-red-100">
+        <div className="App-header">
+          {/* Here u must do smth with svg files, cause there are errors */}
+          {/* <Navbar/> */}
+          {/* <Button spanClass="pl-2" buttonClass="flex p-1 m-2 bg-black bg-opacity-10 rounded-md hover:bg-opacity-20" svg={TableSVG} text="Sample button"/> */}
+          {/* <BoardHeader tableName="Name of table"/> */}
         </div>
-      </DndProvider>
-    );
-  }
-}
+        <div className="App-content flex">
+          {handleLists()}
+          <button
+            className="border-2 px-2 rounded-r-md shadow-md w-1/4 h-8 mt-2 bg-white opacity-70"
+            onClick={handleAddingList}
+          >
+            Add new list
+          </button>
+        </div>
+      </div>
+    </DndProvider>
+  );
+};
 
 export default App;
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,61 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import ListTitle from "./ListTitle";
 import Card from "./Card";
 import SomeContainer from "../SomeContainer";
 
-export default class List extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+const List = (props) => {
+  const [value, setValue] = useState("");
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-  handleSubmit(event) {
-    this.props.handleAddingCard(this.props.listID, this.state.value);
-    this.setState({ value: "" });
+  const handleSubmit = (event) => {
+    props.handleAddingCard(props.listID, value);
+    setValue("");
     event.preventDefault();
-  }
+  };
 
-  handleCards(cards) {
+  const handleCards = (cards) => {
     const tableOfCards = [];
     cards.forEach((item) => {
       tableOfCards.push(
         <Card
           key={item.id}
-          handleRemovingCard={this.props.handleRemovingCard}
-          listID={this.props.listID}
+          handleRemovingCard={props.handleRemovingCard}
+          listID={props.listID}
           cardID={item.id}
           text={item.text}
         />
       );
     });
     return tableOfCards;
-  }
+  };
 
-  render() {
-    return (
-      <div className="list-wrapper w-1/4 bg-gray-200 m-2 h-full">
-        <ListTitle />
-        {this.handleCards(this.props.cards)}
-        <form onSubmit={this.handleSubmit} className="flex">
-          <input
-            className="m-2"
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-            placeholder="Add new card"
-          />
-          <input className="p-1 rounded-md shadow-md" type="submit" value="+" />
-        </form>
-        <SomeContainer />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="list-wrapper w-1/4 bg-gray-200 m-2 h-full">
+      <ListTitle />
+      {handleCards(props.cards)}
+      <form onSubmit={handleSubmit} className="flex">
+        <input
+          className="m-2"
+          type="text"
+          value={value}
+          onChange={handleChange}
+          placeholder="Add new card"
+        />
+        <input className="p-1 rounded-md shadow-md" type="submit" value="+" />
+      </form>
+      <SomeContainer />
+    </div>
+  );
+};
+
+export default List;
